0e036d4c5674a64ae3ac9130f0a24d861f5972b2
zoniventris <f.zoniventris@gmail.com>
2021-10-22 15:08:45 -0700

    Add basic level & player movement.

---
+++ Assets/Scripts/Components/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float maxSpeed = 1.0f;
+
+    private Rigidbody2D rb;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(h, v).normalized * maxSpeed;
+    }
+}
