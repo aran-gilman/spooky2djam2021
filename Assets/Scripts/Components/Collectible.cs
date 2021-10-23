2fa8689630f6d86db69bdecc6e4c46867b123490
zoniventris <f.zoniventris@gmail.com>
2021-10-22 21:29:49 -0700

    Add collectible sounds.

--- Assets/Scripts/Components/Collectible.cs
+++ Assets/Scripts/Components/Collectible.cs
@@ -4,10 +4,19 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Collider2D))]
 public class Collectible : MonoBehaviour
 {
+    public AudioClip collectionSound;
     public UnityEvent onCollect = new UnityEvent();
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (collectionSound != null)
+        {
+            AudioSource audio = other.GetComponent<AudioSource>();
+            if (audio != null)
+            {
+                audio.PlayOneShot(collectionSound);
+            }
+        }
         onCollect.Invoke();
         Destroy(gameObject);
     }
